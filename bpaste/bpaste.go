036064bd07eca5d5ac1ee57aa90a689adc5882a3
Alex Fluter <afluter@gmail.com>
2016-02-03 23:36:53 +0800

    add 3 other paste services, retrieve only
    Signed-off-by: Alex Fluter <afluter@gmail.com>

--- bpaste/bpaste.go
+++ bpaste/bpaste.go
@@ -1,4 +1,4 @@
-package sprunge
+package bpaste
 
 import (
 	"errors"
@@ -6,13 +6,12 @@ import (
 	"io"
 	"io/ioutil"
 	"net/http"
-	"net/url"
 	"regexp"
 )
 
-const SPRUNGE_URL = "http://sprunge.us"
+const BPASTE_URL = "http://bpaste.net"
 
-var re = regexp.MustCompile("http://sprunge.us/([[:alnum:]]+)")
+var re = regexp.MustCompile("https?://bpaste.net/(?:show|raw)/([[:alnum:]]+)")
 
 // Extract sprunge ID from the given URL.
 func GetID(url string) (string, error) {
@@ -20,7 +19,7 @@ func GetID(url string) (string, error) {
 
 	match = re.FindStringSubmatch(url)
 	if len(match) != 2 {
-		return "", errors.New("invalid sprunge url")
+		return "", errors.New("invalid bpaste url")
 	}
 	return match[1], nil
 }
@@ -39,8 +38,7 @@ func Get(id string) (string, error) {
 
 	body, err = ioutil.ReadAll(reader)
 	if err != nil {
-		fmt.Println("read server response error")
-		fmt.Println(err)
+		fmt.Println("read server response error:", err)
 		return "", err
 	}
 	return string(body), nil
@@ -54,7 +52,7 @@ func GetReader(id string) (io.Reader, error) {
 	var resp *http.Response
 	var url string
 
-	url = fmt.Sprintf("%s/%s", SPRUNGE_URL, id)
+	url = fmt.Sprintf("%s/raw/%s", BPASTE_URL, id)
 
 	resp, err = http.Get(url)
 	if err != nil {
@@ -73,32 +71,5 @@ func GetReader(id string) (io.Reader, error) {
 // Send the data string to paste server.
 // Returns the ID of this paste if succeeds.
 func Put(data string) (string, error) {
-	var err error
-	var resp *http.Response
-	var formData url.Values
-	var body []byte
-
-	formData = url.Values{"sprunge": {data}}
-
-	resp, err = http.PostForm(SPRUNGE_URL, formData)
-	if err != nil {
-		fmt.Println("error while sending data to sprunge")
-		fmt.Println(err)
-		return "", err
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != 200 {
-		fmt.Println("server returned an error:")
-		fmt.Println(resp.Status)
-		return "", errors.New(resp.Status)
-	}
-
-	body, err = ioutil.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Println("read server response error")
-		fmt.Println(err)
-		return "", err
-	}
-	return string(body), nil
+	return "", errors.New("Not implemented")
 }
--- paste.go
+++ paste.go
@@ -7,12 +7,18 @@ import (
 	"os"
 	"path/filepath"
 
+	"github.com/fluter01/paste/bpaste"
+	"github.com/fluter01/paste/codepad"
 	"github.com/fluter01/paste/sprunge"
+	"github.com/fluter01/paste/pastebin"
 )
 
 func usage(prog string) {
-	fmt.Printf("Usage:    %s [-h|--help] <file>\n\n"+
+	fmt.Printf("Usage:    %s [-h|--help] -s <service> -g|-p <file>\n\n"+
 		"             -h|--help - Show this help message.\n"+
+		"             -s|--service - Select paste service(Default sprunge).\n"+
+		"             -g|--get - Get the paste.\n"+
+		"             -p|--put - Send the paste.\n"+
 		"             file      - The file that will be sent to paste.\n",
 		filepath.Base(prog))
 	os.Exit(1)
@@ -59,7 +65,7 @@ func processFile(filename string) {
 		return
 	}
 
-	res, err = sprunge.Put(data)
+	res, err = putter(data)
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -67,20 +73,72 @@ func processFile(filename string) {
 	fmt.Println("URL: ", res)
 }
 
+func processID(id string) {
+	var err error
+	var res string
+
+	res, err = getter(id)
+	if err != nil {
+		return
+	}
+
+	fmt.Println(res)
+}
+
+var (
+	getter func(string) (string, error)
+	putter func(string) (string, error)
+)
+
+
 func main() {
 	var help bool
+	var service string
+	var get bool
+	var put bool
 
 	flag.BoolVar(&help, "h", false, "Show help message")
 	flag.BoolVar(&help, "help", false, "Show help message")
+	flag.StringVar(&service, "s", "sprunge", "Paste service")
+	flag.StringVar(&service, "service", "sprunge", "Paste service")
+	flag.BoolVar(&get, "g", false, "Get the paste")
+	flag.BoolVar(&get, "get", false, "Get the paste")
+	flag.BoolVar(&put, "p", false, "Send the paste")
+	flag.BoolVar(&put, "put", false, "Send the paste")
 	flag.Parse()
 
-	if help {
+	if help || (get && put) || !(get || put) {
 		usage(os.Args[0])
 	}
 
-	for i := 0; i < flag.NArg(); i++ {
-		file := flag.Arg(i)
-		processFile(file)
+	switch service[0] {
+	default:
+		fallthrough
+	case 's':
+		getter = sprunge.Get
+		putter = sprunge.Put
+	case 'p':
+		getter = pastebin.Get
+		putter = pastebin.Put
+	case 'c':
+		getter = codepad.Get
+		putter = codepad.Put
+	case 'i':
+	case 'b':
+		getter = bpaste.Get
+		putter = bpaste.Put
+	}
+
+	if put {
+		for i := 0; i < flag.NArg(); i++ {
+			file := flag.Arg(i)
+			processFile(file)
+		}
+	} else {
+		for i := 0; i < flag.NArg(); i++ {
+			id := flag.Arg(i)
+			processID(id)
+		}
 	}
 	return
 }
